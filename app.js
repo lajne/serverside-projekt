7ab940c63391b16a07629ff5cbac34626d4cd960
Alexander Linné <21100826+lajne@users.noreply.github.com>
2019-01-25 14:44:40 +0100

    added nav

--- app.js
+++ app.js
@@ -11,4 +11,21 @@ app.use(express.static("public"))
 app.get('/', function(request, response){
  response.render("page-home.hbs")
 })
+
+app.get('/books', function(request, response){
+  response.render("page-books.hbs")
+ })
+
+ app.get('/authors', function(request, response){
+  response.render("page-authors.hbs")
+ })
+
+ app.get('/admins', function(request, response){
+  response.render("page-administrators.hbs")
+ })
+
+ app.get('/login', function(request, response){
+  response.render("page-login.hbs")
+ })
+ 
 app.listen(8080)
