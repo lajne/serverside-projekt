bfd5d5a9a5a697262731f098ab2277fbad398f91
Alexander Linné <21100826+lajne@users.noreply.github.com>
2019-01-28 11:35:54 +0100

    edit books authors

--- app.js
+++ app.js
@@ -3,57 +3,67 @@ const expressHandlebars = require('express-handlebars')
 const app = express()
 
 const books = [{
+  id: 1,
     title: "Book about A",
     author: "Anders Andersson",
     abstract: "Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.",
     isbn: "1234567890"
   }, {
+    id: 2,
     title: "Book about B",
     author: "Anders Andersson",
     abstract: "Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.",
-    isbn: "1234567890"
+    isbn: "1134567890"
   }, {
+    id: 3,
     title: "Book about C",
     author: "Anders Andersson",
     abstract: "Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.",
-    isbn: "1234567890"
+    isbn: "1334567890"
   }, {
+    id: 4,
     title: "Book about D",
     author: "Anders Andersson",
     abstract: "Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.",
-    isbn: "1234567890"
+    isbn: "1434567890"
   } , {
-  title: "Book about E",
+    id: 5,
+    title: "Book about E",
     author: "Anders Andersson",
     abstract: "Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.",
-    isbn: "1234567890"
+    isbn: "1534567890"
 }]
 
 const authors = [{
+  id: 1,
   surname: "Anders",
   lastname: "Andersson",
   birthdate: "1962-11-24",
   birthplace: "London",
   publisher: "Bonniers"
 }, {
+  id: 2,
   surname: "Anders",
   lastname: "Svensson",
   birthdate: "1963-11-24",
   birthplace: "London",
   publisher: "Publishers CO"
 }, {
+  id: 3,
   surname: "Anders",
   lastname: "Gregersson",
   birthdate: "1964-11-24",
   birthplace: "London",
   publisher: "ABC Bokförlag ÖB"
 }, {
+  id: 4,
   surname: "Anders",
   lastname: "Björnsson",
   birthdate: "1965-11-24",
   birthplace: "London",
   publisher: "Svenssons"
 }, {
+  id: 5,
   surname: "Anders",
   lastname: "Goggenheim",
   birthdate: "1966-11-24",
@@ -73,11 +83,17 @@ app.get('/', function(request, response){
 })
 
 app.get('/books', function(request, response){
-  response.render("page-books.hbs")
+  const model = {
+    books: books
+  }
+  response.render("page-books.hbs", model)
  })
 
  app.get('/authors', function(request, response){
-  response.render("page-authors.hbs")
+   const model = {
+     authors: authors
+   }
+  response.render("page-authors.hbs", model)
  })
 
  app.get('/admins', function(request, response){
@@ -86,6 +102,30 @@ app.get('/books', function(request, response){
 
  app.get('/login', function(request, response){
   response.render("page-login.hbs")
+ })
+
+ app.get('/books/edit/:id', function(request, response){
+   const id = request.params.id
+   const book = books.find(b => b.id == id)
+
+   console.log(book)
+
+   const model = {
+     book: book
+   }
+
+  response.render("page-editbooks.hbs", model)
+ })
+
+ app.get('/authors/edit/:id', function(request, response){
+  const id = request.params.id
+  const author = authors.find(b => b.id == id)
+
+  const model = {
+    author: author
+  }
+
+  response.render("page-editauthors.hbs", model)
  })
  
 app.listen(8080)
