f374e3a34a7c2fac4bb62489ba0c6b96eee05177
Alexander Linné <21100826+lajne@users.noreply.github.com>
2019-01-31 17:22:38 +0100

    refactored a couple of get requests

--- app.js
+++ app.js
@@ -96,6 +96,8 @@ app.get('/', function(request, response){
  response.render("page-home.hbs")
 })
 
+// --------------    //  BOOKs, AUTHORs‚ ADMINs‚ LOGIN  //    -------------- //
+
 app.get('/books', function(request, response){
   const model = {
     books: books
@@ -103,100 +105,52 @@ app.get('/books', function(request, response){
 
   console.log("Not filtered: " + JSON.stringify(model, null, 2))
   response.render("page-books.hbs", model)
- })
+})
 
- app.get('/authors', function(request, response){
-   const model = {
-     authors: authors
-   }
-  response.render("page-authors.hbs", model)
- })
+app.get('/authors', function(request, response){
+  const model = {
+    authors: authors
+  }
+response.render("page-authors.hbs", model)
+})
 
- app.get('/admins', function(request, response){
-   const model = {
-     admins: admins
-   }
-  response.render("page-admins.hbs", model)
- })
+app.get('/admins', function(request, response){
+  const model = {
+    admins: admins
+  }
+response.render("page-admins.hbs", model)
+})
 
- app.get('/login', function(request, response){
-  response.render("page-login.hbs")
- })
+app.get('/login', function(request, response){
+response.render("page-login.hbs")
+})
 
- app.get('/books/:id', function(request, response){
-   const id = request.params.id
-   const book = books.find(b => b.id == id)
+// --------------    //  CREATE  //    -------------- //
 
-   const model = {
-     book: book
-   }
+app.get("/books/create", function(request, response){
+  response.render("page-createbook.hbs")
+})
 
-  response.render("page-viewbooks.hbs", model)
- })
+app.get("/authors/create", function(request, response){
+  response.render("page-createauthor.hbs")
+})
 
- 
- app.get('/authors/:id', function(request, response){
-   const id = request.params.id
-   const author = authors.find(b => b.id == id)
-   
-   const model = {
-     author: author
-    }
-    
-    response.render("page-viewauthors.hbs", model)
-  })
-  
-  app.get('/admins/:id', function(request, response){
-    const id = request.params.id
-    const admin = admins.find(b => b.id == id)
-    
-    const model = {
-      admin: admin
-    }
-    
-    response.render("page-viewadmins.hbs", model)
-  })
-  
-  app.get("/books/:id/edit", function(request, response){
-    response.render("page-editbook.hbs")
-  })
+app.get("/admins/create", function(request, response){
+  response.render("page-createadmin.hbs")
+})
 
-  app.get("/authors/:id/edit", function(request, response){
-    response.render("page-editauthor.hbs")
-  })
+// --------------    //  SEARCHTERM  //    -------------- //
 
-  app.get("/admins/:id/edit", function(request, response){
-    response.render("page-editadmin.hbs")
-  })
-
-  app.get("/books/create", function(request, response){
-    response.render("page-createbook.hbs")
-  })
-
-  app.get("/authors/create", function(request, response){
-    response.render("page-createauthor.hbs")
-  })
-
-  app.get("/admins/create", function(request, response){
-    response.render("page-createadmin.hbs")
-  })
-
- app.get('/books/search', function(request, response){
-   const searchTerm = request.query.bookSearch
-
-   response.redirect("/books/search/" + searchTerm)
- })
-
- app.get('/books/search/:searchTerm', function(request, response){
+app.get('/books/search/:searchTerm', function(request, response){
   const searchTerm = request.params.searchTerm
-
+  
   if(searchTerm == "") {
     const model = {
       books: books
     }
     response.render("page-books.hbs", model)  
   }
-
+  
   let filteredBooksByTitle = []
   for(let book of books) {
     if(book.title.toLowerCase().match(searchTerm.toLowerCase())){
@@ -209,36 +163,96 @@ app.get('/books', function(request, response){
   }
   console.log("Filtered book-object: " + JSON.stringify(model, null, 2))
   response.render("page-books.hbs", model)
- })
-
- app.get('/authors/search', function(request, response){
-  const searchTerm = request.query.authorSearch
-
-  response.redirect("/authors/search/" + searchTerm)
 })
 
 app.get('/authors/search/:searchTerm', function(request, response){
- const searchTerm = request.params.searchTerm
+  const searchTerm = request.params.searchTerm
 
- if(searchTerm == "") {
-   const model = {
-     authors: authors
-   }
-   response.render("page-authors.hbs", model)  
- }
+  if(searchTerm == "") {
+  const model = {
+    authors: authors
+  }
+  response.render("page-authors.hbs", model)  
+  }
 
- let filteredAuthorsByName = []
- for(let author of authors) {
-   if(author.surname.toLowerCase().match(searchTerm.toLowerCase()) || author.lastname.toLowerCase().match(searchTerm.toLowerCase())){
+  let filteredAuthorsByName = []
+  for(let author of authors) {
+  if(author.surname.toLowerCase().match(searchTerm.toLowerCase()) || 
+  author.lastname.toLowerCase().match(searchTerm.toLowerCase())){
     filteredAuthorsByName.push(author)
-   }
- }
+  }
+  }
   console.log("Filtered author-array: " + JSON.stringify(filteredAuthorsByName, null, 2))
   const model = {
       authors: filteredAuthorsByName
     }
- console.log("Filtered author-object: " + JSON.stringify(model, null, 2))
- response.render("page-authors.hbs", model)
+  console.log("Filtered author-object: " + JSON.stringify(model, null, 2))
+  response.render("page-authors.hbs", model)
 })
+
+// --------------    //  SEARCH  //    -------------- //
+
+app.get('/books/search', function(request, response){
+  const searchTerm = request.query.bookSearch
+
+  response.redirect("/books/search/" + searchTerm)
+})
+  
+app.get('/authors/search', function(request, response){
+const searchTerm = request.query.authorSearch
+
+response.redirect("/authors/search/" + searchTerm)
+})
+
+// --------------    //  GET ID  //    -------------- //
+
+app.get('/books/:id', function(request, response){
+  const id = request.params.id
+  const book = books.find(b => b.id == id)
+
+  const model = {
+    book: book
+  }
+
+  response.render("page-viewbooks.hbs", model)
+})
+
+ 
+app.get('/authors/:id', function(request, response){
+  const id = request.params.id
+  const author = authors.find(b => b.id == id)
+  
+  const model = {
+    author: author
+  }
+
+  response.render("page-viewauthors.hbs", model)
+})
+  
+app.get('/admins/:id', function(request, response){
+  const id = request.params.id
+  const admin = admins.find(b => b.id == id)
+  
+  const model = {
+    admin: admin
+  }
+  
+  response.render("page-viewadmins.hbs", model)
+})
+  
+// --------------    //  EDIT  //    -------------- //
+
+app.get("/books/:id/edit", function(request, response){
+  response.render("page-editbook.hbs")
+})
+
+app.get("/authors/:id/edit", function(request, response){
+  response.render("page-editauthor.hbs")
+})
+
+app.get("/admins/:id/edit", function(request, response){
+  response.render("page-editadmin.hbs")
+})
+
  
 app.listen(8080)
